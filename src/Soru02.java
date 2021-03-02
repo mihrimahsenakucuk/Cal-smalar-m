a43cad9c9d0044651becb8f5de666907772ad8ed
sena <mskucukk@outlook.com>
2021-03-02 19:19:42 +0300

    soru2

---
+++ src/Soru02.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Soru02 {
+
+        public static void main(String[] args) {
+            //Kullanıcıdan alınan sayının negatif yada pozitif olduğunu yazdıran programı giriniz.
+
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Bir sayı girin:");
+
+            int sayi = scan.nextInt();
+
+            if (sayi < 0)
+                System.out.println(sayi + " Negatiftir.");
+
+            else if ( sayi > 0)
+                System.out.println(sayi + " Pozitiftir.");
+
+            else
+                System.out.println(sayi + "  Sıfırdır.");
+        }
+    }
+
+
